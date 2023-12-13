226cee161b44e41a842e541d436c80e6d418d6d5
pentaco <pendulum2029@gmail.com>
2023-12-13 22:45:01 +0900

    マイグレーションファイル、モデル、シーダーの作成

---
+++ database/migrations/2023_12_13_130218_create_timeslots_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\TimeslotStatus;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timeslots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('timeslot_status_id')->default(TimeslotStatus::VACANT);
+            $table->foreignId('timetable_id')->default(null)->nullable();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timeslots');
+    }
+};
