76f7fcb1abde1b4545c32aaa02e8016f4abbb6bf
pentaco <pendulum2029@gmail.com>
2023-12-18 08:12:56 +0900

    ディレクトリ名の間違いを修正 Api -> api

--- app/Http/Controllers/api/CancelReservationController.php
+++ app/Http/Controllers/api/CancelReservationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\api;
 
 use App\Models\Reservation;
 use Illuminate\Http\Request;
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StoreReservationController;
-use App\Http\Controllers\Api\CancelReservationController;
+use App\Http\Controllers\api\StoreReservationController;
+use App\Http\Controllers\api\CancelReservationController;
 
 /*
 |--------------------------------------------------------------------------
